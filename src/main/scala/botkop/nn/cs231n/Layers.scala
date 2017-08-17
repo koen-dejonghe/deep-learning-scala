1d7448ee1b0b05219c5c59f6fa4190aa1dc18f62
Koen Dejonghe <koen.dejonghe@cegeka.com>
2017-08-17 19:14:29 +0200

    further impl of layer functions

--- src/main/scala/botkop/nn/cs231n/Layers.scala
+++ src/main/scala/botkop/nn/cs231n/Layers.scala
@@ -2,21 +2,56 @@ package botkop.nn.cs231n
 
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.linalg.ops.transforms.Transforms
 
 object Layers {
 
   class LayerCache
-  case class AffineCache(x: INDArray, w: INDArray, b: INDArray) extends LayerCache
+  case class AffineCache(x: INDArray, w: INDArray, b: INDArray)
+      extends LayerCache
 
-  def affineForward(x: INDArray, w: INDArray, b: INDArray): (INDArray, AffineCache) = {
+  /**
+    *     Computes the forward pass for an affine (fully-connected) layer.
+    *
+    *     The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    *     examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    *     reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    *     then transform it to an output vector of dimension M.
+    *
+    *     Inputs:
+    *     - x: An array containing input data, of shape (N, d_1, ..., d_k)
+    *     - w: An array of weights, of shape (D, M)
+    *     - b: An array of biases, of shape (M,)
+    *
+    *     Returns a tuple of:
+    *     - out: output, of shape (N, M)
+    *     - cache: (x, w, b)
+    */
+  def affineForward(x: INDArray,
+                    w: INDArray,
+                    b: INDArray): (INDArray, AffineCache) = {
     val xs = x.reshape(x.shape()(0), w.shape()(0))
     val out = (xs mmul w) addRowVector b
     val cache = AffineCache(x, w, b)
     (out, cache)
   }
 
-
-  def affineBackward(dout: INDArray, cache: AffineCache): (INDArray, INDArray, INDArray) = {
+  /**
+    *     Computes the backward pass for an affine layer.
+    *
+    *     Inputs:
+    *     - dout: Upstream derivative, of shape (N, M)
+    *     - cache: Tuple of:
+    *     - x: Input data, of shape (N, d_1, ... d_k)
+    *     - w: Weights, of shape (D, M)
+    *
+    *     Returns a tuple of:
+    *     - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    *     - dw: Gradient with respect to w, of shape (D, M)
+    *     - db: Gradient with respect to b, of shape (M,)
+    */
+  def affineBackward(dout: INDArray,
+                     cache: AffineCache): (INDArray, INDArray, INDArray) = {
     import cache._
 
     val mul = dout mmul w.transpose()
@@ -30,4 +65,140 @@ object Layers {
     (dx, dw, db)
   }
 
+  /**
+    *     Computes the forward pass for a layer of rectified linear units (ReLUs).
+    *
+    *     Input:
+    *     - x: Inputs, of any shape
+    *
+    *     Returns a tuple of:
+    *     - out: Output, of the same shape as x
+    *     - cache: x
+    */
+  def reluForward(x: INDArray): (INDArray, INDArray) = {
+    val out = Transforms.max(x, 0.0)
+    val cache = x
+    (out, cache)
+  }
+
+  /**
+    *     Computes the backward pass for a layer of rectified linear units (ReLUs).
+    *
+    *     Input:
+    *     - dout: Upstream derivatives, of any shape
+    *     - cache: Input x, of same shape as dout
+    *
+    *     Returns:
+    *     - dx: Gradient with respect to x
+    */
+  def reluBackward(dout: INDArray, cache: INDArray): INDArray = {
+    val x = cache.data().asDouble().map(d => if (d <= 0.0) 0.0 else 1.0)
+    val dx = dout mul Nd4j.create(cache.shape, x)
+    dx
+  }
+
+  /**
+    *     Computes the loss and gradient using for multiclass SVM classification.
+    *
+    *     Inputs:
+    *     - x: Input data, of shape (N, C) where x[i, j] is the score for the jth
+    *       class for the ith input.
+    *     - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    *       0 <= y[i] < C
+    *
+    *     Returns a tuple of:
+    *     - loss: Scalar giving the loss
+    *     - dx: Gradient of the loss with respect to x
+    */
+  def svmLoss(x: INDArray, y: INDArray): (Double, INDArray) = {
+
+    val n = x.shape()(0).toDouble
+    val xData = x.data().asDouble()
+    val yData = y.data().asInt()
+
+    val xRows = xData.grouped(x.shape()(1))
+
+    val margins = xRows
+      .zip(yData.iterator)
+      .map {
+        case (row, correctIndex) =>
+          val correctScore = row(correctIndex)
+          row.zipWithIndex.map {
+            case (d, i) =>
+              if (i == correctIndex)
+                0.0
+              else
+                Math.max(0.0, d - correctScore + 1.0)
+          }
+      }
+      .toArray
+
+    val loss = margins.flatten.sum / n
+
+    val numPos = margins.map { row =>
+      row.count(_ > 0.0)
+    }
+
+    val dxData = margins.zipWithIndex.map {
+      case (row, rowId) =>
+        val correctIdx = yData(rowId)
+        val np = numPos(rowId)
+        val dRow: Array[Double] = row.map { d =>
+          if (d > 0.0)
+            1.0
+          else
+            0.0
+        }
+        dRow(correctIdx) -= np
+        dRow.map(_ / n)
+    }
+
+    val dx = Nd4j.create(dxData).reshape(x.shape(): _*)
+    (loss, dx)
+  }
+
+  /**
+    *     Computes the loss and gradient for softmax classification.
+    *
+    *     Inputs:
+    *     - x: Input data, of shape (N, C) where x[i, j] is the score for the jth
+    *       class for the ith input.
+    *     - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
+    *       0 <= y[i] < C
+    *
+    *     Returns a tuple of:
+    *     - loss: Scalar giving the loss
+    *     - dx: Gradient of the loss with respect to x
+    */
+  def softmaxLoss(x: INDArray, y: INDArray): (Double, INDArray) = {
+
+    val shiftedLogits = x subColumnVector Nd4j.max(x, 1)
+    val z = Nd4j.sum(Transforms.exp(shiftedLogits), 1)
+    val logProbs = shiftedLogits subColumnVector Transforms.log(z)
+    val probs = Transforms.exp(logProbs)
+    val n = x.shape()(0).toDouble
+    val loss = logProbs
+      .data()
+      .asDouble()
+      .grouped(x.shape()(1))
+      .zip(y.data.asInt.iterator)
+      .foldLeft(0.0) {
+        case (acc, (row, index)) =>
+          acc - row(index)
+      } / n
+
+    val dxData = probs.data.asDouble
+      .grouped(x.shape()(1))
+      .zip(y.data.asInt.iterator)
+      .map {
+        case (row, correctIndex) =>
+          row(correctIndex) -= 1.0
+          row.map(_ / n)
+      }
+      .toArray
+
+    val dx = Nd4j.create(dxData).reshape(x.shape(): _*)
+    (loss, dx)
+
+  }
 }
--- src/test/scala/botkop/nn/cs231n/LayersSpec.scala
+++ src/test/scala/botkop/nn/cs231n/LayersSpec.scala
@@ -1,13 +1,17 @@
 package botkop.nn.cs231n
 
+import org.nd4j.linalg.api.buffer.DataBuffer
+import org.nd4j.linalg.api.buffer.util.DataTypeUtil
+import org.nd4j.linalg.api.iter.NdIndexIterator
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.ops.transforms.Transforms
+import org.nd4j.linalg.indexing.NDArrayIndex
 import org.scalatest.{FlatSpec, Matchers}
-import collection.JavaConverters._
-
 
 class LayersSpec extends FlatSpec with Matchers {
+
+  // must set data type to double for numerical gradient checking
+  DataTypeUtil.setDTypeForContext(DataBuffer.Type.DOUBLE)
 
   "A Layer" should "correctly compute the affine forward pass" in {
 
@@ -18,17 +22,21 @@ class LayersSpec extends FlatSpec with Matchers {
     val inputSize = numInputs * inputShape.product
     val weightSize = outputDim * inputShape.product
 
-    val x = Nd4j.linspace(-0.1, 0.5, inputSize).reshape(numInputs +: inputShape: _*)
-    val w = Nd4j.linspace(-0.2, 0.3, weightSize).reshape(inputShape.product, outputDim)
+    val x =
+      Nd4j.linspace(-0.1, 0.5, inputSize).reshape(numInputs +: inputShape: _*)
+    val w = Nd4j
+      .linspace(-0.2, 0.3, weightSize)
+      .reshape(inputShape.product, outputDim)
     val b = Nd4j.linspace(-0.3, 0.1, outputDim)
 
     val out = Layers.affineForward(x, w, b)._1
 
-    val correctData = Array(1.49834967, 1.70660132, 1.91485297, 3.25553199, 3.5141327, 3.77273342)
+    val correctData = Array(1.49834967, 1.70660132, 1.91485297, 3.25553199,
+      3.5141327, 3.77273342)
     val correctOut = Nd4j.create(correctData).reshape(numInputs, outputDim)
 
     val error = relError(out, correctOut)
-    error should be < 1e-7
+    error should be < 1e-9
 
   }
 
@@ -42,37 +50,163 @@ class LayersSpec extends FlatSpec with Matchers {
 
     val cache = Layers.affineForward(x, w, b)._2
     val derivatives = Layers.affineBackward(dout, cache)
+    val dx = derivatives._1
+    val dw = derivatives._2
+    val db = derivatives._3
 
-    // println(derivatives)
+    def fdx(a: INDArray) = Layers.affineForward(a, w, b)._1
+    def fdw(a: INDArray) = Layers.affineForward(x, a, b)._1
+    def fdb(a: INDArray) = Layers.affineForward(x, w, a)._1
 
-    // test gradient on x
-    val h = 1e-5
+    val dxNum = evalNumericalGradientArray(fdx, x, dout)
+    val dwNum = evalNumericalGradientArray(fdw, w, dout)
+    val dbNum = evalNumericalGradientArray(fdb, b, dout)
+
+    val dxError = relError(dx, dxNum)
+    val dwError = relError(dw, dwNum)
+    val dbError = relError(db, dbNum)
+
+    dxError should be < 1e-9
+    dwError should be < 1e-9
+    dbError should be < 1e-9
+  }
+
+  it should "correctly compute the relu forward pass" in {
+
+    val x = Nd4j.linspace(-0.5, 0.5, 12).reshape(3, 4)
+    val out = Layers.reluForward(x)._1
+
+    val correctData = Array(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.04545455,
+      0.13636364, 0.22727273, 0.31818182, 0.40909091, 0.5)
+    val correctOut = Nd4j.create(correctData).reshape(x.shape(): _*)
+    val error = relError(out, correctOut)
+    error should be < 6e-8
+  }
+
+  it should "correctly compute the relu backward pass" in {
+    Nd4j.getRandom.setSeed(231)
+    val x = Nd4j.randn(10, 10)
+    val dout = Nd4j.randn(x.shape)
+
+    def fdx(a: INDArray) = Layers.reluForward(x)._1
+
+    val dxNum = evalNumericalGradientArray(fdx, x, dout)
+    val cache = Layers.reluForward(x)._2
+    val dx = Layers.reluBackward(dout, cache)
+    val dxError = relError(dx, dxNum)
+
+    dxError should be < 1e-11
+  }
+
+  it should "correctly compute the svm loss and gradient" in {
+    Nd4j.getRandom.setSeed(231)
+    val numClasses = 10
+    val numInputs = 50
+
+    val x = Nd4j.randn(numInputs, numClasses) mul 0.001
+    val y = Nd4j.rand(numClasses, numInputs)
+
+    def fdx(a: INDArray) = Layers.svmLoss(x, y)._1
+    val dxNum = evalNumericalGradient(fdx, x)
+
+    val result = Layers.svmLoss(x, y)
+    val loss = result._1
+    val dx = result._2
+
+    loss should equal (9.0 +- 0.2)
+
+    val dxError = relError(dx, dxNum)
+    dxError should be < 1.5e-9
+
+  }
+
+  it should "correctly compute the softmax loss and gradient" in {
+    Nd4j.getRandom.setSeed(231)
+    val numClasses = 10
+    val numInputs = 50
+
+    val x = Nd4j.randn(numInputs, numClasses) mul 0.001
+    val y = Nd4j.rand(numClasses, numInputs)
+
+    def fdx(a: INDArray) = Layers.softmaxLoss(x, y)._1
+    val dxNum = evalNumericalGradient(fdx, x)
+
+    val result = Layers.softmaxLoss(x, y)
+    val loss = result._1
+    val dx = result._2
+
+    loss should equal (2.3 +- 0.2)
+
+    val dxError = relError(dx, dxNum)
+    dxError should be < 1e-7
+  }
+
+  /**
+    * Evaluate a numeric gradient for a function that accepts an array and returns an array.
+    */
+  def evalNumericalGradientArray(f: (INDArray) => INDArray,
+                                 x: INDArray,
+                                 df: INDArray,
+                                 h: Double = 1e-5): INDArray = {
 
     val grad = Nd4j.zeros(x.shape(): _*)
-    import org.nd4j.linalg.api.iter.NdIndexIterator
     val iter = new NdIndexIterator(x.shape(): _*)
-    while(iter.hasNext) {
-      val nextIndex: Array[Int] = iter.next
-      val oldVal: Double = x.getDouble(nextIndex: _*)
-      x.put(nextIndex, Nd4j.create(Array(oldVal + h)))
-      val pos = Layers.affineForward(x, w, b)._1
-      x.put(nextIndex, Nd4j.create(Array(oldVal - h)))
-      val neg = Layers.affineForward(x, w, b)._1
+    while (iter.hasNext) {
+      val nextIter = iter.next
+      val ii = NDArrayIndex.indexesFor(nextIter: _*)
 
-      x.put(Nd4j.create(nextIndex), oldVal))
+      val oldVal = x.getDouble(nextIter: _*)
 
-      grad.put(nextIndex, Nd4j.sum((pos sub neg) mul dout) div (2.0 * h))
+      x.put(ii, oldVal + h)
+      val pos = f(x)
+
+      x.put(ii, oldVal - h)
+      val neg = f(x)
+
+      x.put(ii, oldVal)
+      val g = Nd4j.sum((pos sub neg) mul df) div (2.0 * h)
+      grad.put(ii, g)
     }
 
-    val dxError = relError(grad, derivatives._1)
-    println(dxError)
+    grad
+  }
+
+  def evalNumericalGradient(f: (INDArray) => Double,
+                            x: INDArray,
+                            h: Double = 0.00001): INDArray = {
+
+    val grad = Nd4j.zeros(x.shape(): _*)
+    val iter = new NdIndexIterator(x.shape(): _*)
+    while (iter.hasNext) {
+      val nextIter = iter.next
+      val ii = NDArrayIndex.indexesFor(nextIter: _*)
+
+      val oldVal = x.getDouble(nextIter: _*)
+
+      x.put(ii, oldVal + h)
+      val pos = f(x)
+
+      x.put(ii, oldVal - h)
+      val neg = f(x)
+
+      x.put(ii, oldVal)
+
+      val g = (pos - neg) / (2.0 * h)
+      grad.put(ii, g)
+    }
+
+    grad
   }
 
   /**
     * returns relative error
     */
-  def relError(x: INDArray, y: INDArray): Double =
-    Nd4j.max(Transforms.abs(x sub y) div (Transforms.abs(x) add Transforms.abs(y))).data().asDouble()(0)
+  def relError(x: INDArray, y: INDArray): Double = {
+    import org.nd4j.linalg.ops.transforms.Transforms._
+    val n = abs(x sub y)
 
+    val d = max(abs(x.dup()) add abs(y.dup()), 1e-8)
+    Nd4j.max(n div d).getDouble(0)
+  }
 
 }
